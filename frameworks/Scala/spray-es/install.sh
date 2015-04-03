a8f66bb29f6cc10640a768494f0049ca7966ce12
marko <marko@markos-MacBook-Pro.local>
2015-04-03 12:10:10 +0300

    travis testing

--- frameworks/Scala/spray-es/install.sh
+++ frameworks/Scala/spray-es/install.sh
@@ -6,5 +6,5 @@ export JAVA_HOME=/opt/java8
 export PATH=$JAVA_HOME/bin:$PATH:$IROOT/sbt/bin
 
 cd $TROOT
-sbt -J-Xmx2G package
-sbt -J-Xmx2G assembly
+sbt -J-Xmx2G -v package
+sbt -J-Xmx2G -v assembly
